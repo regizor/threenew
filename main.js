6e31bee0b84d2086607b2e1788ae7338fc880d45
regizor <vlad.ungureanu77@gmail.com>
2024-02-26 17:34:07 +0200

    added cream model and simple rotation animation

--- main.js
+++ main.js
@@ -1,36 +1,57 @@
 import * as THREE from 'three';
-import { gsap } from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { gsap } from 'gsap';
 
-gsap.registerPlugin(ScrollTrigger);
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-const renderer = new THREE.WebGLRenderer({ alpha: true }); // { alpha: true } for transparent background, if needed
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById('app').appendChild(renderer.domElement);
 
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // Soft white light
+scene.add(ambientLight);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xFF0000 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+directionalLight.position.set(0, 1, 1); // Adjust direction as needed
+scene.add(directionalLight);
+
+
+// Create a GLTFLoader to load the .glb file
+let modelCrema;
+
+const loader = new GLTFLoader();
+loader.load(
+  'models/cream.glb', // Path to your GLB model, relative to the public directory
+  function (gltf) {
+    modelCrema = gltf.scene;
+    scene.add(modelCrema);
+    console.log("Model loaded!");
+    animateModel();
+  },
+  undefined, // You can define a function for progress updates here
+  function (error) {
+    console.error('An error happened while loading the model:', error);
+  }
+);
+
 
 camera.position.z = 5;
 
-// GSAP Animation tied to scroll
-gsap.to(cube.rotation, {
-  scrollTrigger: {
-    trigger: "#large",
-    start: "top top",
-    end: "bottom bottom",
-    scrub: true, // Smooth scrubbing, consider a number for more controlled scrubbing speed
-  },
-  y: Math.PI * 2, // Full rotation
-  x: Math.PI * 2, // Full rotation
-  ease: "none"
-});
+function animateModel() {
+  // Ensure the model is defined
+  if (!modelCrema) return;
+
+  // GSAP animation to rotate the model
+  gsap.to(modelCrema.rotation, {
+    duration: 5, // Duration of the animation in seconds
+    y: Math.PI * 2, // Rotate the model 360 degrees around the Y axis
+    repeat: -1, // Repeat the animation indefinitely
+    ease: "none" // Use a linear ease for constant rotation speed
+  });
+}
+
+
 
 function animate() {
   requestAnimationFrame(animate);
