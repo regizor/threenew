fdcf59a00d818ed5619a2e8a1a57c41f9a49e36d
regizor <vlad.ungureanu77@gmail.com>
2024-02-26 16:53:21 +0200

    rotating at scroll

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 import * as THREE from 'three';
+import { gsap } from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ alpha: true }); // { alpha: true } for transparent background, if needed
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+document.getElementById('app').appendChild(renderer.domElement);
+
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xFF0000 });
@@ -14,12 +19,21 @@ scene.add(cube);
 
 camera.position.z = 5;
 
+// GSAP Animation tied to scroll
+gsap.to(cube.rotation, {
+  scrollTrigger: {
+    trigger: "#large",
+    start: "top top",
+    end: "bottom bottom",
+    scrub: true, // Smooth scrubbing, consider a number for more controlled scrubbing speed
+  },
+  y: Math.PI * 2, // Full rotation
+  x: Math.PI * 2, // Full rotation
+  ease: "none"
+});
+
 function animate() {
   requestAnimationFrame(animate);
-
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
   renderer.render(scene, camera);
 }
 
