5fecfcf2e6054d10ed6a2b5cdd8f8b5b11f5546a
regizor <vlad.ungureanu77@gmail.com>
2024-02-26 13:31:31 +0200

    color change

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const material = new THREE.MeshBasicMaterial({ color: 0xFF0000 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
