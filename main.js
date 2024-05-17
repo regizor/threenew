ad4d498e5e743b78d0610d2866277056194ea34f
regizor <vlad.ungureanu77@gmail.com>
2024-05-17 17:11:08 +0300

    absolute link new

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ let modelCrema;
 
 const loader = new GLTFLoader();
 loader.load(
-  '/models/cream.glb', // Path to your GLB model, relative to the public directory
+  'https://master--spectacular-cupcake-7ecc17.netlify.app/assets/models/cream.glb', // Path to your GLB model, relative to the public directory
   function (gltf) {
     modelCrema = gltf.scene;
     scene.add(modelCrema);
